6e706077f4afa0664f9aa8f1bccb20807df10bf2
taflynn <t.flynn@ncl.ac.uk>
2021-11-30 18:06:50 +0000

    correcting energy function as it was just the same as chem pot

--- energy.py
+++ energy.py
@@ -18,8 +18,8 @@ def energy_eqm_dens_lck(phi,V,r,dr,N_lck):
     dphi_dr = np.gradient(phi,dr)
     E_ke = 4*pi*np.trapz(r**2*0.5*np.abs(dphi_dr)**2)*dr 
     E_pot = 4*pi*np.trapz(r**2*V*np.abs(phi)**2)*dr
-    E_int = 4*pi*np.trapz(r**2*(-3*N_lck*np.abs(phi)**4))*dr
-    E_lhy = 4*pi*np.trapz(r**2*(-2.5*N_lck**1.5*np.abs(phi)**2.5))*dr
+    E_int = 2.0*(4*pi*np.trapz(r**2*(-3*N_lck*np.abs(phi)**4))*dr)
+    E_lhy = 0.4*(4*pi*np.trapz(r**2*(-2.5*N_lck**1.5*np.abs(phi)**2.5))*dr)
                           
     return E_ke,E_pot,E_int,E_lhy
 
@@ -43,17 +43,17 @@ def energy_eqm_dens_ulck(psi1,psi2,r,V1,V2,dr,alpha,beta,eta,N1,N2):
     dpsi1_dr = np.gradient(psi1,dr)
     E1_ke = 4*pi*np.trapz(r**2*0.5*np.abs(dpsi1_dr)**2)*dr 
     E1_pot = 4*pi*np.trapz(r**2*V1*np.abs(psi1)**2)*dr
-    E1_int = 4*pi*np.trapz(r**2*(N1*np.abs(psi1)**4 \
-                         + N2*eta*np.abs(psi1)**2*np.abs(psi2)**2))*dr
-    E1_lhy = 4*pi*np.trapz(r**2*(alpha*(N1*np.abs(psi1)**2 \
-                         + N2*beta*np.abs(psi2)**2)**1.5*np.abs(psi1)**2))*dr
+    E1_int = 2.0*(4*pi*np.trapz(r**2*(N1*np.abs(psi1)**4 \
+                         + N2*eta*np.abs(psi1)**2*np.abs(psi2)**2))*dr)
+    E1_lhy = 0.4*(4*pi*np.trapz(r**2*(alpha*(N1*np.abs(psi1)**2 \
+                         + N2*beta*np.abs(psi2)**2)**1.5*np.abs(psi1)**2))*dr)
     
     dpsi2_dr = np.gradient(psi2,dr)
     E2_ke = 4*pi*np.trapz(r**2*0.5*np.abs(dpsi2_dr)**2)*dr 
     E2_pot = 4*pi*np.trapz(r**2*V2*np.abs(psi2)**2)*dr
-    E2_int = 4*pi*np.trapz(r**2*(N2*beta*np.abs(psi2)**4 \
-                         + N1*eta*beta*np.abs(psi1)**2*np.abs(psi2)**2))*dr
-    E2_lhy = 4*pi*np.trapz(r**2*(alpha*beta**2*(N1*np.abs(psi1)**2 \
-                         + N2*beta*np.abs(psi2)**2)**1.5*np.abs(psi2)**2))*dr
+    E2_int = 2.0*(4*pi*np.trapz(r**2*(N2*beta*np.abs(psi2)**4 \
+                         + N1*eta*beta*np.abs(psi1)**2*np.abs(psi2)**2))*dr)
+    E2_lhy = 0.4*(4*pi*np.trapz(r**2*(alpha*beta**2*(N1*np.abs(psi1)**2 \
+                         + N2*beta*np.abs(psi2)**2)**1.5*np.abs(psi2)**2))*dr)
     
     return E1_ke,E1_pot,E1_int,E1_lhy,E2_ke,E2_pot,E2_int,E2_lhy
