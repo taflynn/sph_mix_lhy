fixing the units code, missing hbar in tau defn

init.py
@@ -26,6 +26,10 @@ def grid_setup(dr,Nr,DT_COEF):
     # time step
     dt = DT_COEF*dr**2
     
+    print('Initialising system...')
+    print('Box size = ',Lr,' with dr = ',dr,' and dt = ',dt)
+    print(67*'-')
+    
     return Lr,r,dt
 
 def init_wavefun_dens_lck(r,dr,GAUSS_SIGMA,INIT_TYPE):
@@ -44,17 +48,21 @@ def init_wavefun_dens_lck(r,dr,GAUSS_SIGMA,INIT_TYPE):
     The super-Gaussian form can give a significant benefit in convergence time for the flat-topped droplet form. The 
     function only outputs phi, i.e., the initial wavefunction profile.
     """
+    
+    print('Density-locked mixture using initial condition:')
+    
     if INIT_TYPE == 'GAUSS':
         # Gaussian initial condition
         phi = np.exp(-(r)**2/(2*(GAUSS_SIGMA)**2))
+        print('Gaussian')
     elif INIT_TYPE == 'S_GAUSS':
         # Gaussian initial condition
         phi = np.exp(-((r)**2/(2*(GAUSS_SIGMA)**2))**3)
-
+        print('Super-Gaussian')
     # normalise initial condition
     Norm = 4*pi*np.trapz(r**2*np.abs(phi)**2)*dr
     phi = phi/np.sqrt(Norm)
-
+    print(67*'-')
     return phi
 
 def init_wavefun_dens_ulck(r,dr,GAUSS_SIGMA,INIT_TYPE1,INIT_TYPE2):
@@ -73,16 +81,22 @@ def init_wavefun_dens_ulck(r,dr,GAUSS_SIGMA,INIT_TYPE1,INIT_TYPE2):
     The super-Gaussian form can give a significant benefit in convergence time for the flat-topped droplet form. The 
     function only outputs phi, i.e., the initial wavefunction profile.
     """
+    
+    print('Density-unlocked mixture using initial condition:') 
+
     # choosing initial condition (1st-component)
     if INIT_TYPE1 == 'GAUSS':
         # Gaussian initial condition (1st-component)
         psi1 = np.exp(-(r)**2/(2*(GAUSS_SIGMA)**2)) 
+        print('Gaussian in 1st component')
     elif INIT_TYPE1 == 'S_GAUSS':
         # Super-Gaussian initial condition
         psi1 = np.exp(-((r)**2/(2*(GAUSS_SIGMA)**2))**3)
+        print('Super-Gaussian in 1st component')
     elif INIT_TYPE1 == 'NON_ZERO_TAIL':
         # Super-Gaussian initial condition w/ non-zero tail (1st-component)
         psi1 = np.exp(-((r)**2/(2*(GAUSS_SIGMA)**2))**3) + 0.1*np.ones(len(r))
+        print('Super-Gaussian w/ non-zero density tail in 1st component')
     
     # normalise initial condition (1st-component)
     Norm = 4*pi*np.trapz(r**2*abs(psi1)**2)*dr
@@ -92,17 +106,20 @@ def init_wavefun_dens_ulck(r,dr,GAUSS_SIGMA,INIT_TYPE1,INIT_TYPE2):
     if INIT_TYPE2 == 'GAUSS':
         # Gaussian initial condition (2nd-component)
         psi2 = np.exp(-(r)**2/(2*(GAUSS_SIGMA)**2)) 
+        print('Gaussian in 2nd component')
     elif INIT_TYPE2 == 'S_GAUSS':
         # Super-Gaussian initial condition (2nd-component)
         psi2 = np.exp(-((r)**2/(2*(GAUSS_SIGMA)**2))**3)
+        print('Super-Gaussian in 2nd component')
     elif INIT_TYPE2 == 'NON_ZERO_TAIL':
         # Super-Gaussian initial condition w/ non-zero tail (2nd-component)
         psi2 = np.exp(-((r)**2/(2*(GAUSS_SIGMA)**2))**3) + 0.0001*np.ones(len(r))
+        print('Super-Gaussian w/ non-zero density tail in 1st component')
 
     # normalise initial condition (2nd-component)
     Norm = 4*pi*np.trapz(r**2*abs(psi2)**2)*dr
     psi2 = psi2/np.sqrt(Norm) 
-    
+    print(67*'-')
     return psi1,psi2
 
 def potential_dens_lck(r,OMEGA):
@@ -112,6 +129,12 @@ def potential_dens_lck(r,OMEGA):
     trapping can be varied by OMEGA. If no trap is desired, simply set OMEGA = 0.
     """
     V = 0.5*OMEGA**2*r**2
+    
+    if OMEGA == 0:
+        print('No trapping potential on density-locked mixture')
+    elif OMEGA != 0:
+        print('Harmonic trapping potential with frequency, ',OMEGA,', on density-locked mixture')
+    print(67*'-')
     return V
 
 def potential_dens_ulck(r,OMEGA1,OMEGA2):
@@ -122,4 +145,17 @@ def potential_dens_ulck(r,OMEGA1,OMEGA2):
     """
     V1 = 0.5*OMEGA1**2*r**2
     V2 = 0.5*OMEGA2**2*r**2
+    
+    if OMEGA1 == 0 and OMEGA2 == 0:
+        print('No trapping potential on density-unlocked mixture')
+    elif OMEGA1 == 0 and OMEGA2 != 0 :
+        print('No trapping potential on component 1')
+        print('and harmonic trapping potential with frequency, ',OMEGA2,', on component 2')
+    elif OMEGA1 != 0 and OMEGA2 == 0 :
+        print('Harmonic trapping potential with frequency, ',OMEGA1,', on component 1')
+        print('and no trapping potential on component 2')
+    elif OMEGA1 != 0 and OMEGA2 != 0:
+        print('Harmonic trapping potential with frequency, ',OMEGA1,', on component 1')
+        print('and harmonic trapping potential with frequency, ',OMEGA2,', on component 2')
+    print(67*'-')
     return V1,V2

params_calc.py
@@ -3,7 +3,7 @@ from numpy import pi
 from equil_dens import eq_dens_lck
 from units import units,natural_consts
 
-def dens_lck_params(m1,m2,a11,a22,a12,N):
+def params_dens_lck(m1,m2,a11,a22,a12,N):
     """
     This function calculates the necessary dimensionless parameter for the density-locked mixture. 
     
@@ -37,9 +37,16 @@ def dens_lck_params(m1,m2,a11,a22,a12,N):
         g22 = 4*pi*hbar**2*a22/m2
         N_lck = (N/(n01*xi**3))*(np.sqrt(g22)/(np.sqrt(g11) + np.sqrt(g22)))
     
+    print('Calculating defining parameters and scales for density locked mixture:')
+    print('Effective atom number of density-locked mixture, N_lck = ',N_lck)
+    print('Lengthscale, xi = ',xi)
+    print('Timescale, tau = ',tau)
+    print('Equilibrium density of component 1, n01 = ',n01)
+    print('Equilibrium density of component 2, n02 = ',n02)
+    print(67*'-')
     return N_lck,xi,tau,n01,n02
 
-def eqm_dens_ulck_params(m1,m2,a11,a22,a12,N1,N2):
+def params_dens_ulck(m1,m2,a11,a22,a12,N1,N2):
     """
     This function calculates the necessary dimensionless parameters for the density-unlocked mixture (though this is 
     currently only for the equal masses mixture). The outputs are hence:
@@ -55,7 +62,13 @@ def eqm_dens_ulck_params(m1,m2,a11,a22,a12,N1,N2):
     -> a12 - the interspecies scattering length
     -> N1,N2 - the two component atom numbers
     """
+    
+    # natural constants
+    [hbar,a0,Da] = natural_consts()
+    
     if m1 == m2:
+        a11 = a11*a0; a22 = a22*a0; a12 = a12*a0
+        m1 = m1*Da; m2 = m2*Da
         # equilibrium densities equal masses solving coupled equations
         #n01,n02 = eq_dens_unlck_eqm(m1,m2,a11,a22,a12)
         
@@ -79,5 +92,14 @@ def eqm_dens_ulck_params(m1,m2,a11,a22,a12,N1,N2):
     elif m1 != m2:
         # equilibrium densities unequal masses
         print('Density-unlocked and unequal masses is not yet available')
+    
+    print('Calculating defining parameters and scales for density-unlocked mixture:')
+    print('Dimensionless parameters of mixture:')
+    print('alpha = ',alpha, ', beta = ',beta,', eta = ',eta)
+    print('Lengthscale, xi = ',xi)
+    print('Timescale, tau = ',tau)
+    print('Equilibrium density of component 1, n01 = ',n01)
+    print('Equilibrium density of component 2, n02 = ',n02)
+    print(67*'-')
     
     return alpha,beta,eta,xi,tau,n01,n02,rho1,rho2,N1,N2

units.py
@@ -2,6 +2,9 @@ import numpy as np
 from numpy import pi
 
 def units(m1,m2,a11,a22,a12,n01):
+    """
+    
+    """
     # natural constants
     [hbar,a0,Da] = natural_consts()
 
@@ -11,7 +14,7 @@ def units(m1,m2,a11,a22,a12,n01):
         
         # length and time scaling
         xi = np.sqrt((3/(8*pi))*((np.sqrt(a11) + np.sqrt(a22))/(np.abs(delta_a)*np.sqrt(a11)*n01)))
-        tau = (3*m/(8*pi))*((np.sqrt(a11) + np.sqrt(a22))/(np.abs(delta_a)*np.sqrt(a11)*n01))
+        tau = (3*m/(8*pi*hbar))*((np.sqrt(a11) + np.sqrt(a22))/(np.abs(delta_a)*np.sqrt(a11)*n01))
         
     elif m1 != m2:
         # interaction strengths
@@ -27,8 +30,14 @@ def units(m1,m2,a11,a22,a12,n01):
     return xi,tau
 
 def natural_consts():
+    """
+    The natural constants used throughout defining the parameters in these equations:
+    -> hbar - Planck's constant on 2\pi
+    -> a0 - Bohr radius used in defining the scattering length
+    -> Da - Dalton used in defining the unified atomic mass
+    """
     # natural constants
-    hbar = 1.0545718*1e-34
-    a0 = 5.29*1e-11 # Bohr radius
+    hbar = 1.054571817*1e-34
+    a0 = 5.29177210903*1e-11
     Da = 1.66053906660*1e-27
     return hbar,a0,Da