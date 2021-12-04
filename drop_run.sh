9090b0a35d6978b4ac00ffe486b4e65a5bf9e6c0
taflynn <73589300+taflynn@users.noreply.github.com>
2021-12-04 10:00:11 +0000

    module load matplotlib

--- drop_run.sh
+++ drop_run.sh
@@ -2,8 +2,6 @@
 # 
 #SBATCH -t 00:05:00
 #SBATCH -p defq
-#SBATCH --ntasks = 1
-#SBATCH --cpus-per-task = 1
-#SBATCH --ntasks-per-core = 1
 module load Python
+module load matplotlib/3.2.1-foss-2020a-Python-3.8.2
 python3 running.py
