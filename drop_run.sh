10675e402096b6573ace706dcec76843f707d0a3
taflynn <73589300+taflynn@users.noreply.github.com>
2021-12-06 09:58:41 +0000

    output the print commands

--- drop_run.sh
+++ drop_run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # 
-#SBATCH -t 00:05:00
 #SBATCH -p defq
 module load Python
-module load matplotlib/3.2.1-foss-2020a-Python-3.8.2
-python3 running.py
+module load matplotlib/3.0.0-foss-2018b-Python-3.6.6
+python3 -u running.py
