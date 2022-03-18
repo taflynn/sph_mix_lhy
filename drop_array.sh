e59f802d641bac500d99a5aa99f7b6e992cf3563
taflynn <t.flynn@ncl.ac.uk>
2022-03-18 11:18:07 +0000

    improving workflow scripts

--- drop_array.sh
+++ drop_array.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# set up a job array with tasks numbered 1 to 10.
-#SBATCH --array=1-10
+# set up a job array with tasks numbered 1 to n.
 #
 # give the array a single job name
 #SBATCH -J array_sph_drop
 #
 # Standard error messages are saved in this file
-#SBATCH -e drop_array_%A_%a.err
+#SBATCH -e job_array${SLURM_ARRAY_TASK_ID}.err
 #
 # request one core per task
 #SBATCH -c 1
@@ -16,4 +15,4 @@ module load Python
 module load matplotlib/3.0.0-foss-2018b-Python-3.6.6
 #
 # use the task ID to locate the input file for the task.
-python3 running.py -wp ./data/drop_array_%A_%a -rp ./${SLURM_ARRAY_TASK_ID}.json
+python3 running.py -wp $1${SLURM_ARRAY_TASK_ID} -rp ./config_dens_$2${SLURM_ARRAY_TASK_ID}.json
--- generator_imbal.sh
+++ generator_imbal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 x=1
-for i in $1 $2 $3 $4 $5
+for i in $(seq $1 $2 $3);
 do
     cp ./config_dens_ulck.json ./config_dens_ulck$x.json
     awk -v val=$i '{ gsub("\"N1\":[0-9]+","\"N1\":"val,$0);print $0}' ./config_dens_ulck.json > ./config_dens_ulck$x.json
