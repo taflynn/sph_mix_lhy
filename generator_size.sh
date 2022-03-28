37ebb226d8fbc8e5a4346b8a4afdba58da8096ee
taflynn <t.flynn@ncl.ac.uk>
2022-03-28 14:58:52 +0100

    changing the structure of the droplet size data generator shell script

--- generator_size.sh
+++ generator_size.sh
@@ -3,6 +3,10 @@ x=1
 for i in $(seq $1 $2 $3);
 do
     cp ./config_dens_ulck.json ./config_dens_ulck$x.json
-    awk -v val=$i '{ gsub("\"a12\":-[0-9]+","\"a12\":-"val,$0);print $0}' ./config_dens_ulck.json > ./config_dens_ulck$x.json
+    touch ./config_dens_ulcktemp.json
+    let "imb = $i / $4 + $i"
+    awk -v val=$imb '{ gsub("\"N1\":[0-9]+","\"N1\":"val,$0);print $0}' ./config_dens_ulck.json > ./config_dens_ulcktemp.json
+    awk -v val=$i '{ gsub("\"N2\":[0-9]+","\"N2\":"val,$0);print $0}' ./config_dens_ulcktemp.json > ./config_dens_ulck$x.json
     ((x = x + 1)) 
 done
+rm ./config_dens_ulcktemp.json
