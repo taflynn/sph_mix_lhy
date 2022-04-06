54216f03011d173c1ca95953a680b1eece19e192
taflynn <t.flynn@ncl.ac.uk>
2022-04-06 11:25:27 +0100

    changing shell script so that it varies GAUSS_SIGMA1

--- generator_size.sh
+++ generator_size.sh
@@ -8,7 +8,7 @@ do
     cp ./config_dens_ulck.json ./config_dens_ulck$x.json
     touch ./config_dens_ulcktemp1.json
     touch ./config_dens_ulcktemp2.json
-    awk -v val=$gauss_sig '{ gsub("\"GAUSS_SIGMA\":[0-9]+.0","\"GAUSS_SIGMA\":"val,$0);print $0}' ./config_dens_ulck.json > ./config_dens_ulcktemp1.json
+    awk -v val=$gauss_sig '{ gsub("\"GAUSS_SIGMA1\":[0-9]+.0","\"GAUSS_SIGMA1\":"val,$0);print $0}' ./config_dens_ulck.json > ./config_dens_ulcktemp1.json
     let "imb = $i / $6 + $i"
     awk -v val=$imb '{ gsub("\"N1\":[0-9]+","\"N1\":"val,$0);print $0}' ./config_dens_ulcktemp1.json > ./config_dens_ulcktemp2.json
     awk -v val=$i '{ gsub("\"N2\":[0-9]+","\"N2\":"val,$0);print $0}' ./config_dens_ulcktemp2.json > ./config_dens_ulck$x.json
